f56f984766db824d41c5675056221c3c77b47cd9
Dat <thanhdat2002tn@gmail.com>
2024-08-22 16:14:29 +0700

    update

--- src/main/java/WhileDemo.java
+++ src/main/java/WhileDemo.java
@@ -1,6 +1,6 @@
 public class WhileDemo {
     public static void main(String[] args) {
-        int i = 100;
+        int i = 1;
         while (i <= 10) {
             System.out.println(i++);
         }
